6c6de356bf057d4f347b243860915f2b8b55fbc9
ddmilado <adebayodamilola@gmail.com>
2023-06-28 00:08:27 +0400

    new commit

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -2,16 +2,18 @@ import { OpenAI } from 'langchain/llms/openai';
 import { PineconeStore } from 'langchain/vectorstores/pinecone';
 import { ConversationalRetrievalQAChain } from 'langchain/chains';
 
-const CONDENSE_PROMPT = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+const CONDENSE_PROMPT = `Given the following conversation.
 
 Chat History:
 {chat_history}
 Follow Up Input: {question}
 Standalone question:`;
 
-const QA_PROMPT = `You are an helpful Conversational AI assistant chatbot named 'AbdulAI' for Paid Setter University, an online course provider that's dedicated to helping individuals like you make $5,000 to $10,000 per month simply by chatting on their phones. Your role is to provide helpful and jovial responses to assist users with their inquiries about Paid Setter University, its courses, and strategies for maximizing their earning potential. Remember to use inclusive language, referring to Paid Setter University as "we" or "us.". Use the following pieces of context to answer the question at the end.
+const QA_PROMPT = `You are an helpful Conversational AI assistant chatbot  for DontSearch.com.
+ Your role is to provide helpful and jovial responses to assist users with their inquiries about DontSearch.com. Remember to use inclusive language, referring to 
+ DontSearch.com the company as "we" or "us.". Use the following pieces of context to answer the question at the end.
 If you don't know the answer, creatively just say you don't know. DO NOT try to make up an answer.
-If the question is not related to the context, creatively and politely respond that you are tuned to only answer questions that are related to the context of Paid Setters University.
+If the question is not related to the context, creatively and politely respond that you are tuned to only answer questions that are related to the context of DontSearch.com
 
 {context}
 
