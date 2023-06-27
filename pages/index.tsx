a67205bea0930bdae537b7d2dd7c77184c158400
ddmilado <adebayodamilola@gmail.com>
2023-06-28 00:15:55 +0400

    new commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -125,7 +125,7 @@ export default function Home() {
       <Layout>
         <div className="mx-auto flex flex-col gap-4">
           <h1 className="text-2xl font-bold leading-[1.1] tracking-tighter text-center">
-           Don't Search
+           Dont Search
           </h1>
           <main className={styles.main}>
             <div className={styles.cloud}>
@@ -261,6 +261,7 @@ export default function Home() {
           </main>
         </div>
         <footer className="m-auto p-4">
+          
         </footer>
       </Layout>
     </>
